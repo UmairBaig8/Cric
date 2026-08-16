02224dd9336b15f9fa8efe35f3af1b41c9130ee6
Umair Baig <umairbaig808@gmail.com>
2026-08-17 04:49:57 +0530

    Employee ID as 7-8 digit number, fix migration backfill

--- supabase/migrations/20260818000000_add_employee_id_rename_department.sql
+++ supabase/migrations/20260818000000_add_employee_id_rename_department.sql
@@ -1,13 +1,6 @@
 alter table public.registrations
   add column if not exists employee_id text;
 
-update public.registrations
-set employee_id = upper(email)
-where employee_id is null;
-
-alter table public.registrations
-  alter column employee_id set not null;
-
 alter table public.registrations
   rename column department to squad;
 
